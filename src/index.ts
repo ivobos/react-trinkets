c7a74bfc271188b61b86dfc0716b9c2ba52d643b
Ivo Bosticky <ivobos@gmail.com>
2024-06-20 00:07:02 +1000

    fix typo in export

--- src/index.ts
+++ src/index.ts
@@ -12,7 +12,7 @@ export { default as PasswordStrengthMeter } from "./PasswordStrengthMeter";
 export { default as PixelArtGenerator } from "./PixelArtGenerator";
 export { default as PrimeNumberCheck } from "./PrimeNumberCheck";
 export { default as SierpinskiTriangle } from "./SierpinskiTriangle";
-export { default as SockTicker } from "./StockTicker";
+export { default as StockTicker } from "./StockTicker";
 export { default as VirtualAquarium } from "./VirtualAquarium";
 export { default as WeatherIcon } from "./WeatherIcon";
 
